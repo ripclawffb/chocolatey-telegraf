bd2644cca9e60f94b7e09111fc766ddd27c4e9be
Simon Baerlocher <s.baerlocher@sbaerlocher.ch>
2020-01-30 17:34:54 +0100

    fixed not working

--- tools/chocolateybeforemodify.ps1
+++ tools/chocolateybeforemodify.ps1
@@ -1,7 +1,3 @@
 If (Get-Service -Name "telegraf" -ErrorAction SilentlyContinue) {
     Stop-Service -Name "telegraf"
 }
-
-If (Test-Path "$env:ProgramFiles\telegraf\telegraf.conf" -ErrorAction SilentlyContinue) {
-    Copy-Item -Force -Path "$env:ProgramFiles\telegraf\telegraf.conf" -Destination "$env:ProgramFiles\telegraf\telegraf.backup.conf"
-}
